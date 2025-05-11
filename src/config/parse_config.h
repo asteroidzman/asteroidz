DreamMaoMao/maomaowm

feat: create virtual monitor

src/config/parse_config.h
@@ -525,6 +525,8 @@ FuncType parse_func_name(char *func_name, Arg *arg, char *arg_value,
     (*arg).ui = 1 << (atoi(arg_value2) - 1);
   } else if (strcmp(func_name, "quit") == 0) {
     func = quit;
+  } else if (strcmp(func_name, "cmd_create_output") == 0) {
+    func = cmd_create_output;
   } else if (strcmp(func_name, "moveresize") == 0) {
     func = moveresize;
     (*arg).ui = parse_mouse_action(arg_value);

src/dispatch/dispatch.h
@@ -55,3 +55,4 @@ void movewin(const Arg *arg);
 void resizewin(const Arg *arg);
 void toggle_named_scratch(const Arg *arg);
 void toggle_render_border(const Arg *arg);
+void cmd_create_output(const Arg *arg);

asteroidzman/asteroidz

src/config/parse_config.h
@@ -525,6 +525,10 @@ FuncType parse_func_name(char *func_name, Arg *arg, char *arg_value,
     (*arg).ui = 1 << (atoi(arg_value2) - 1);
   } else if (strcmp(func_name, "quit") == 0) {
     func = quit;
+  } else if (strcmp(func_name, "create_virtual_output") == 0) {
+    func = create_virtual_output;
+  } else if (strcmp(func_name, "destroy_all_virtual_output") == 0) {
+    func = destroy_all_virtual_output;
   } else if (strcmp(func_name, "moveresize") == 0) {
     func = moveresize;
     (*arg).ui = parse_mouse_action(arg_value);

src/dispatch/dispatch.h
@@ -55,3 +55,5 @@ void movewin(const Arg *arg);
 void resizewin(const Arg *arg);
 void toggle_named_scratch(const Arg *arg);
 void toggle_render_border(const Arg *arg);
+void create_virtual_output(const Arg *arg);
+void destroy_all_virtual_output(const Arg *arg);