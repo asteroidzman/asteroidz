8b3ce7b5806ea68ec142d2b9369f19eb98eebb4e
DreamMaoMao <2523610504@qq.com>
2025-10-18 12:43:03 +0800

    project: add mmsg as sub project

--- format.sh
+++ format.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-clang-format -i src/*/*.h -i  src/*/*.c -i  src/mango.c
+clang-format -i src/*/*.h -i  src/*/*.c -i  src/mango.c -i mmsg/src/mmsg.c -i mmsg/src/arg.h -i mmsg/src/dynarr.h 
