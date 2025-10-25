97c7553c38e627ddb1f8ab599b4fc9d27d38987c
DreamMaoMao <2523610504@qq.com>
2025-10-25 14:38:52 +0800

    opt: dont set border for fullscreen window when reload config

--- src/config/parse_config.h
+++ src/config/parse_config.h
@@ -3055,7 +3055,7 @@ void reapply_border(void) {
 	// reset border width when config change
 	wl_list_for_each(c, &clients, link) {
 		if (c && !c->iskilling) {
-			if (!c->isnoborder) {
+			if (!c->isnoborder && !c->isfullscreen) {
 				c->bw = borderpx;
 			}
 		}
