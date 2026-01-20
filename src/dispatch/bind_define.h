31f57562d4d2d8a341164af3ed2313870993af11
DreamMaoMao <2523610504@qq.com>
2026-01-20 13:50:43 +0800

    opt: only disable animation for resizewin tiling window

--- src/dispatch/bind_define.h
+++ src/dispatch/bind_define.h
@@ -383,9 +383,6 @@ int32_t movewin(const Arg *arg) {
 	if (!c->isfloating)
 		togglefloating(NULL);
 
-	int32_t animations_state_backup = animations;
-	animations = 0;
-
 	switch (arg->ui) {
 	case NUM_TYPE_MINUS:
 		c->geom.x -= arg->i;
@@ -413,7 +410,6 @@ int32_t movewin(const Arg *arg) {
 	c->iscustomsize = 1;
 	c->float_geom = c->geom;
 	resize(c, c->geom, 0);
-	animations = animations_state_backup;
 	return 0;
 }
 
@@ -431,7 +427,8 @@ int32_t resizewin(const Arg *arg) {
 		return 0;
 
 	int32_t animations_state_backup = animations;
-	animations = 0;
+	if (!c->isfloating)
+		animations = 0;
 
 	if (ISTILED(c)) {
 		switch (arg->ui) {
@@ -585,9 +582,6 @@ int32_t smartmovewin(const Arg *arg) {
 	nx = c->geom.x;
 	ny = c->geom.y;
 
-	int32_t animations_state_backup = animations;
-	animations = 0;
-
 	switch (arg->i) {
 	case UP:
 		tar = -99999;
@@ -674,7 +668,6 @@ int32_t smartmovewin(const Arg *arg) {
 		.x = nx, .y = ny, .width = c->geom.width, .height = c->geom.height};
 	c->iscustomsize = 1;
 	resize(c, c->float_geom, 1);
-	animations = animations_state_backup;
 	return 0;
 }
 
@@ -689,9 +682,6 @@ int32_t smartresizewin(const Arg *arg) {
 		setfloating(c, true);
 	nw = c->geom.width;
 	nh = c->geom.height;
-
-	int32_t animations_state_backup = animations;
-	animations = 0;
 
 	switch (arg->i) {
 	case UP:
@@ -748,7 +738,6 @@ int32_t smartresizewin(const Arg *arg) {
 		.x = c->geom.x, .y = c->geom.y, .width = nw, .height = nh};
 	c->iscustomsize = 1;
 	resize(c, c->float_geom, 1);
-	animations = animations_state_backup;
 	return 0;
 }
 
